7b65cc5e96a9cc03669ddb8055e403c6cb66da68
Phongbui <phongprosupper@gmail.com>
2025-06-01 19:57:11 +0700

    finish ex3

---
+++ homework_oop/hus/oop/final_2022_2023_matrix_integration_studentmanager/integration/Integrator.java
@@ -0,0 +1,5 @@
+package hus.oop.final_2022_2023_matrix_integration_studentmanager.integration;
+
+public interface Integrator {
+    double integrate(Polynomial poly, double lower, double upper);
+}
---
+++ homework_oop/hus/oop/final_2022_2023_matrix_integration_studentmanager/integration/TrapezoidRule.java
@@ -0,0 +1,57 @@
+package hus.oop.final_2022_2023_matrix_integration_studentmanager.integration;
+
+public class TrapezoidRule implements Integrator {
+    private double precision;
+    private int maxIterations;
+
+    public TrapezoidRule(double precision, int maxIterations) {
+        /* TODO */
+        this.precision = precision;
+        this.maxIterations = maxIterations;
+    }
+
+    /**
+     * Phương thức tính xấp xỉ giá trị tích phân. Giá trị xấp xỉ được chấp nhận nếu phép tính đạt độ chính xác đã cho,
+     * hoặc có số vòng vượt quá ngưỡng quy định.
+     * Độ chính xác được xác định như sau, chọn n0 tùy ý, sau đó tính I_n với n = n0, 2n0, 4n0, ...
+     * Việc tính toán dừng lại khi |I_2n - In|/3 < eps (precision), hoặc số lần chia đôi vượt quá ngưỡng quy định (maxIterations).
+     * @param poly
+     * @param lower
+     * @param upper
+     * @return
+     */
+    @Override
+    public double integrate(Polynomial poly, double lower, double upper) {
+        /* TODO */
+        int n = 1;
+        double In = integrate(poly, lower, upper, n);
+        for (int iter = 1; iter <= maxIterations; iter++) {
+            n *= 2;
+            double I2n = integrate(poly, lower, upper, n);
+            if (Math.abs(I2n - In) / 3.0 < precision) {
+                return I2n;
+            }
+            In = I2n;
+        }
+        return In;
+    }
+
+    /**
+     * Phương thức tính xấp xỉ giá trị tích phân với numOfSubIntervals khoảng phân hoạch đều.
+     * @param poly
+     * @param lower
+     * @param upper
+     * @param numOfSubIntervals
+     * @return giá trị xấp xỉ giá trị tích phân.
+     */
+    private double integrate(Polynomial poly, double lower, double upper, int numOfSubIntervals) {
+        /* TODO */
+        double h = (upper - lower) / numOfSubIntervals;
+        double sum = 0.5 * (poly.evaluate(lower) + poly.evaluate(upper));
+        for (int i = 1; i < numOfSubIntervals; i++) {
+            double x = lower + i * h;
+            sum += poly.evaluate(x);
+        }
+        return sum * h;
+    }
+}
